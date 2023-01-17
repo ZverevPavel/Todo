5a4bc41d6338f62af37bf3da84db323bb0725a25
ZverevPavel <zzzverevv@yandex.ru>
2023-01-17 21:12:45 +0500

    task_4

--- Usersapp/views.py
+++ Usersapp/views.py
@@ -1,8 +1,10 @@
-from rest_framework.viewsets import ModelViewSet
+from rest_framework import viewsets
+from rest_framework import mixins
 from .models import User
 from .serializers import UserModelSerializer
 
 
-class UserModelViewSet(ModelViewSet):
+class UserCustomViewSet(mixins.ListModelMixin, mixins.RetrieveModelMixin, viewsets.GenericViewSet):
     queryset = User.objects.all()
     serializer_class = UserModelSerializer
+    renderer_classes = [JSONRenderer, BrowsableAPIRenderer]
