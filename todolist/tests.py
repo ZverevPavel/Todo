8733b3a042a019c08fcfe96cbe30cd8d4b7acff2
ZverevPavel <95680990+ZverevPavel@users.noreply.github.com>
2023-02-05 13:39:00 +0500

    task_8 (#8)

--- todolist/tests.py
+++ todolist/tests.py
@@ -1,3 +1,17 @@
+import json
 from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APIRequestFactory, force_authenticate, APIClient, APISimpleTestCase, APITestCase
+from mixer.backend.django import mixer
+from django.contrib.auth.models import User
+from .views import TodoModelViewSet
+from .models import Todo, Project
 
-# Create your tests here.
+
+class TestProjectViewSet(TestCase):
+    def test_get_list(self):
+        factory = APIRequestFactory()
+        request = factory.get('/api/projects/')
+        view = ProjectViewSet.as_view({'get': 'list'})
+        response = view(request)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
